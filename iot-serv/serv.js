6ab27a70bb3a7e2f9c88e82dc49cd05917390a50
DonatienLinossier <donatien.linossier@gmail.com>
2025-04-08 00:33:57 +0200

    serv.js : first test for database connection. Functional but dirty code.

--- iot-serv/serv.js
+++ iot-serv/serv.js
@@ -1,5 +1,16 @@
 const axios = require('axios');
 const express = require('express');
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'fde_database',
+});
+
+
 
 //Data that we ll have to query from the DB
 const apis = [
@@ -9,7 +20,13 @@ const apis = [
     { "id": 3, "url": "http://127.0.0.1:8003", "apikey": "key4", "lastDataTime": null }
 ];
 
-let data = {};
+var dataG = {};
+apis.forEach(api => {
+    dataG[api.id] = [];
+});
+
+
+var data = {};
 
 apis.forEach(api => {
     data[api.id] = [];
@@ -24,6 +41,8 @@ const getData = async () => {
             });
 
             data[api.id].push(response.data);
+            dataG[api.id].push(response.data);
+            //console.log(data[api.id]);
 
         } catch (error) {
             console.error(api)
@@ -31,6 +50,32 @@ const getData = async () => {
             console.error(error.response?.data || error.message);
         }
     }
+
+    const sql = 'INSERT INTO production SET ?';
+
+    for (const api of apis) {
+        const entries = data[api.id];
+        if (!entries || entries.length === 0) continue;
+
+        for (const entry of entries) {
+            const prod = {
+                ID_power_source: api.id,
+                date_prod: entry.date,
+                targeted_prod: entry.targeted_exploitation ?? undefined,
+                actual_prod: entry.actual_exploitation ?? undefined
+            };
+
+            connection.query(sql, prod, (err, result) => {
+                if (err) {
+                    console.error('Insert error:', err);
+                }
+            });
+        }
+    }
+    data = {};
+    apis.forEach(api => {
+        data[api.id] = [];
+    });
 };
 
 setInterval(getData, 1000);
@@ -39,7 +84,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.json(data);
+    res.json(dataG);
 });
 
 app.listen(PORT, () => {
