1917e7f63221795b2a4aba7acf14f4adabaaf3a8
Thomas R <totovadordlp@gmail.com>
2025-04-20 21:14:43 +0200

    User connection saved and redirections are done (pages are accessible only if user is connected or not) + logout problem solved.

--- prod/server.js
+++ prod/server.js
@@ -45,33 +45,8 @@ app.use('/', loginRouter);
 const personalAccountRouter = require('./routes/personalAccount');
 app.use('/', personalAccountRouter);
 
-app.get('/logout', (req, res) => {
-  console.log("\nPage : /logout");
-  //console.log("Variables de session : ", req.session);
-  
-  if (req.session) {
-      console.log("Tentative de déconnexion.");
-      req.session.destroy(err => {
-          if (err) {
-              req.session.error_msg = "Erreur destruction session !";
-              console.log(req.session.error_msg);
-              return res.redirect('/homepage');
-          }
-          res.clearCookie('connect.sid');   // supprime les cookies cote client
-          console.log("ID de cookie nettoyé avec succès : res.clearCookie('connect.sid');");
-          console. log("Utilisateur déconnecté.")
-          return res.redirect('/');
-      });
-  }
-  else {
-      console.log("Pas de session à détruire");
-      res.send('Pas de session à détruire');
-  }
-});
-
-/*
 const logoutRouter = require('./routes/logout');
-app.use('/', logoutRouter);*/
+app.use('/', logoutRouter);
 
 
 
