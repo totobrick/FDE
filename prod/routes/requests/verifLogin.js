0575a5bbb8bb607c96e3386c4dcde7ed7feb715f
DonatienLinossier <donatien.linossier@gmail.com>
2025-04-29 00:53:28 +0200

    Use of fetch to show informations to the user while loggingin

--- prod/routes/requests/verifLogin.js
+++ prod/routes/requests/verifLogin.js
@@ -3,21 +3,17 @@ const sql = require('mysql2');
 const router = express.Router();
 const {queryPromise} = require("./../functions/functions.js");
 const bcrypt = require('bcrypt');
+const multer = require('multer');
+const upload = multer();
 
 
-
-router.post('/requests/verifLogin', async (req, res) => {
-    console.log("\nPage : /verifLogin");
-
+router.post('/requests/verifLogin', upload.none(), async (req, res) => {
     const login = req.body.login;   //name of the input field
     const pwd = req.body.password;  //name of the input field
-    // try/catch : used in case of failure for the database connection or bad request
+
     try {
         const query = "SELECT ID, isValidated, isSuperAdmin, password FROM user WHERE BINARY login=?";
         const response = await queryPromise(query, [login]);
-        
-        console.log("Nb lignes : ", response.length);
-        console.log("response", response);
 
         // Un utilisateur trouvé dans la database
         if (response.length == 1){
@@ -25,25 +21,17 @@ router.post('/requests/verifLogin', async (req, res) => {
             const match = await bcrypt.compare(pwd, response[0].password);
 
             if(!match) {
-                return res.redirect(301, '/login');
+                return res.status(200).json({ message: "Mauvais mot de passe."});
             }
-
-            console.log("RESPONSE : ", response);
 
             // Vérifie si le compte a été validé par le superAdministrateur
             if (response[0].isValidated == 0){
-                req.session.error_msg = "Votre compte n'a pas encore été validé par l'Administrateur. Un peu de patience ...";
-                console.log(req.session.error_msg);
-                return res.redirect(301, '/login');
+                return res.status(200).json({ message: "Votre compte n'a pas encore été validé par l'Administrateur. Un peu de patience ..."});
             }
 
             // Enregistre si l'utilisateur est un superAdmin
-            if (response[0].isSuperAdmin != 0){
-                console.log("response[0].isSuperAdmin = ", response[0].isSuperAdmin);
-
-                // l'utilisateur est un superAdmin
+            if (response[0].isSuperAdmin != 0) {
                 req.session.isSuperAdmin = "1";
-                console.log("req.session.isSuperAdmin = ", req.session.isSuperAdmin);
             }
 
             // Variables de session utilisateur
@@ -54,27 +42,22 @@ router.post('/requests/verifLogin', async (req, res) => {
             req.session.cache_buster = Date.now();
 
             if (! req.session.login){
-                console.log("LOGIN : Pas de variable de session.");
-                return;
+                return res.status(200).json({ message: "Internal Error."});
             }
 
-            console.log("LOGIN : variable de session existante.");
-            return res.redirect(301, "/homepage");        // 301 : http status for permanent redirection
+            return res.status(200).json({ redirect: "/homepage"});
         }
         else if (response.length >= 2){
             console.log("Erreur anormal : 2 utilisateurs possèdent le même login et mot de passe.");
-            req.session.error_msg = "Erreur anormal : 2 utilisateurs possèdent le même login et mot de passe.";
-            return res.redirect("/login");
+            return res.status(200).json({ message: "Internal Error."});
         }
         else{
-            console.log("Login ou mot de passe incorrect !");
-            req.session.error_msg = "Login ou mot de passe incorrect !";
-            return res.redirect("/login");
+            return res.status(200).json({ message: "Utilisateur introuvable."});
         }
     }
     catch (err) {
-        console.error("Erreur dans la route :", err);
-        res.status(500).send("Erreur serveur");
+        console.error(err);
+        return res.status(200).json({ message: "Internal Error."});
     }
 });
 
